4f6feb019e70dfd81c15dce97fb6f6cca3c52415
elliotbohlen <bohle000@newschool.edu>
2020-12-17 17:27:17 -0500

    working on glitch

--- content.js
+++ content.js
@@ -1,27 +1,107 @@
-//  //needs document end
-// var elements = document.getElementsByTagName('*');
+document.onmousemove = function mousePosition(event) {
+    // let x = event.offsetX;
+    // let y = event.offsetY;
 
-// for (var i = 0; i < elements.length; i++) {
-//     var element = elements[i];
-//     //console.log(element);
-//     //element.style.setProperty('border', 'solid 1px red');
+    // document.getElementById("positions").insertAdjacentHTML('afterbegin', ` 
+    // <div class="trail" style= "
+    // left:${x}px;  
+    // top:${y}px;
+    // font-size: ${y/20}px;
+    // ">(${x+40},${y+40})
+    // </div>  `);
 
-//     var r = Math.floor(Math.random() * 255);
-//     var g = Math.floor(Math.random() * 255);
-//     var b = Math.floor(Math.random() * 255);
+    var elements = document.getElementsByTagName("*");
 
-//     var c = "rgb(" + r + "," + g + "," + b + ")";
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        // let x = event.pageX;
+        // let y = event.pageY;
 
-//     element.style.setProperty('background-color', c);
+        // var xpos = x + "px";
+        // var ypos = y + "px";
+        // element.style.setProperty('left', xpos);
+        // element.style.setProperty('top', ypos);
 
-// }
+        element.style.left = (event.pageX / 10) + "%";
+        element.style.top = (event.pageY / 10) + "%";
 
-// text replacer, needs document_idle in manifest (everything finished loading)
-var html = document.querySelector('html');
-var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT); //only getting text nodes (cool command)
 
-var node;
-
-while (node = walker.nextNode()) {
-    node.nodeValue = node.nodeValue.replace(/trump/gi, 'some guy'); //g stands for global, (all of them), i means case insensitive, this is whats called a regular expression
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  //needs document end
+// // var elements = document.getElementsByTagName('*');
+
+// // for (var i = 0; i < elements.length; i++) {
+// //     var element = elements[i];
+// //     //console.log(element);
+// //     //element.style.setProperty('border', 'solid 1px red');
+
+// //     var r = Math.floor(Math.random() * 255);
+// //     var g = Math.floor(Math.random() * 255);
+// //     var b = Math.floor(Math.random() * 255);
+
+// //     var c = "rgb(" + r + "," + g + "," + b + ")";
+
+// //     element.style.setProperty('background-color', c);
+
+// // }
+
+// // text replacer, needs document_idle in manifest (everything finished loading)
+// // var html = document.querySelector('html');
+// // var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT); //only getting text nodes (cool command)
+
+// // var node;
+
+// // while (node = walker.nextNode()) {
+// //     node.nodeValue = node.nodeValue.replace(/trump/gi, 'some guy'); //g stands for global, (all of them), i means case insensitive, this is whats called a regular expression
+// // }
+
+// // var elements = document.getElementsByTagName('*');
+
+// // for (var i = 0; i < elements.length; i++) {
+// //     var element = elements[i];
+// //     //console.log(element);
+// //     //element.style.setProperty('border', 'solid 1px red');
+
+// //     var date = new Date();
+// //     var hours = date.getHours();
+// //     var minutes = date.getMinutes();
+// //     var seconds = date.getSeconds();
+// //     console.log(minutes)
+
+// //     var c = "rgb(" + 9 + "," + 9 + "," + seconds + ")";
+
+// //     element.style.setProperty('background-color', c);
+// // };
+
+// setInterval(function() {
+//     var date = new Date();
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var seconds = date.getSeconds();
+//     console.log(minutes)
+
+//     var c = "rgb(" + hours + "," + 255 + "," + minutes + ")";
+// }, 1000);
+// // check query syntax
+// document.querySelector("div").style.setProperty('background-color', c);
